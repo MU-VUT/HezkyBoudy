528b7f7760d7e10099e900728993fb1756fa7232
MU VUT <161508@vutbr.cz>
2022-06-30 11:19:56 +0200

    Gallery completed

--- script.js
+++ script.js
@@ -7,3 +7,13 @@
 // document.getElementById("nav").appendChild(navigationHTML);
 
 //Navigation animation
+
+//Get dimensions from gallery
+for (let i = 0; i < 10; i++) {
+  let img = document.createElement("img");
+  img.onload = function (event) {
+    console.log("Image: r1" + i);
+    console.log("width,height:", img.width, img.height);
+  };
+  img.src = "/res/img/real4/r1" + i + ".JPG";
+}
